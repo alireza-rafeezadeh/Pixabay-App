cb676db293e8b083107f21a1637c1c72e8c7aaa6
Alireza Rafeezadeh <rafeezadeh.alireza@gmail.com>
2021-07-10 21:29:12 +0430

    PIX-2 Show data from server into list

--- app/src/main/java/com/app/pixabay/presentation/ui/search/SearchFragment.kt
+++ app/src/main/java/com/app/pixabay/presentation/ui/search/SearchFragment.kt
@@ -2,13 +2,14 @@ package com.app.pixabay.presentation.ui.search
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.app.pixabay.R
 import com.app.pixabay.databinding.FragmentSearchBinding
 import com.app.pixabay.presentation.util.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import com.app.core.domain.ResultWrapper
+import com.app.core.domain.search.SearchResponse
 
 @AndroidEntryPoint
 class SearchFragment : Fragment(R.layout.fragment_search) {
@@ -23,7 +24,24 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
 
     private fun observeInFragment() {
         viewModel.searchImageLiveData.observe(viewLifecycleOwner, {
-            Toast.makeText(requireContext(), "$it", Toast.LENGTH_SHORT).show()
+
+            when (it) {
+                is ResultWrapper.Success -> {
+                    initRecyclerView(it.data)
+                }
+                is ResultWrapper.ErrorString -> {
+
+                }
+                is ResultWrapper.InProgress -> {
+
+                }
+            }
+
         })
+    }
+
+    private fun initRecyclerView(data: SearchResponse) {
+        val adapter = SearchRVAdapter(data.hits)
+        binding.searchRecyclerView.adapter = adapter
     }
 }
